ce12009238183cffd2a4b91a753775ca4be4c070
Dimulski <george.dimulski@gmail.com>
2022-03-19 23:06:11 +0200

    [master] Add redux devtools and explore plugin

--- the-ultimate-redux-course/redux-starter/src/index.js
+++ the-ultimate-redux-course/redux-starter/src/index.js
@@ -50,13 +50,28 @@
 // console.log(updated);
 
 
-import store from './customStore';
-import * as actions from './actions'
+// import store from './customStore';
+// import * as actions from './actions'
+
+// store.subscribe(() => {
+//   console.log("Store changed")
+// })
+
+// store.dispatch(actions.bugAdded("Bug 1"))
+
+// console.log(store.getState())
+
+import store from './store';
+import * as actions from './actions';
 
 store.subscribe(() => {
-  console.log("Store changed")
-})
+  console.log("Store changed");
+});
 
-store.dispatch(actions.bugAdded("Bug 1"))
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 
-console.log(store.getState())
+
+console.log(store.getState());
--- the-ultimate-redux-course/redux-starter/src/store.js
+++ the-ultimate-redux-course/redux-starter/src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-const store = createStore(reducer) // createStore is a HOF
+const store = createStore(
+  reducer,
+  devToolsEnhancer({ trace: true })
+) // createStore is a HOF
 
 export default store;

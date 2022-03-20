66d881a44f3ea99636798da6b59eddf57f76da19
Dimulski <george.dimulski@gmail.com>
2022-03-20 16:51:05 +0200

    [master] Test out Redux Toolkit createReducer and replace logic with createSlice

--- the-ultimate-redux-course/redux-starter/src/index.js
+++ the-ultimate-redux-course/redux-starter/src/index.js
@@ -70,10 +70,10 @@ store.subscribe(() => {
   console.log("Store changed");
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
-store.dispatch(actions.bugAdded("Bug 2"));
-store.dispatch(actions.bugAdded("Bug 3"));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 
 console.log(store.getState());

d4081c40911c480f980502bd59f7398f6ee3206a
Dimulski <george.dimulski@gmail.com>
2022-04-10 18:17:52 +0300

    [master] Add default Thunk middleware

--- the-ultimate-redux-course/redux-starter/src/index.js
+++ the-ultimate-redux-course/redux-starter/src/index.js
@@ -84,31 +84,45 @@
 // console.log(x === y);
 
 
-import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, bugAssigned, getBugsByUser } from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
+// import configureStore from './store/configureStore';
+// import { bugAdded, bugResolved, bugAssigned, getBugsByUser } from './store/bugs';
+// import { projectAdded } from './store/projects';
+// import { userAdded } from './store/users';
 
+// const store = configureStore();
+
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1"}));
+// store.dispatch(bugAdded({ description: "Bug 2"}));
+// store.dispatch(bugAdded({ description: "Bug 3"}));
+// store.dispatch(bugAdded({ description: "Bug 4"}));
+// store.dispatch(bugAdded({ description: "Bug 5"}));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(userAdded({ name: "Georgi" }));
+// store.dispatch(userAdded({ name: "Shéyaa" }));
+// store.dispatch(bugAssigned({ bugId: 1, userId: 1 }))
+// store.dispatch(bugAssigned({ bugId: 2, userId: 1 }))
+// store.dispatch(bugAssigned({ bugId: 2, userId: 2 }))
+// store.dispatch(bugAssigned({ bugId: 3, userId: 2 }))
+// store.dispatch(bugAssigned({ bugId: 4, userId: 2 }))
+// store.dispatch(bugAssigned({ bugId: 5, userId: 2 }))
+
+// const x = getBugsByUser(1)(store.getState())
+// const y = getBugsByUser(2)(store.getState())
+
+// console.log(x);
+// console.log(y);
+
+
+// Dispatching functions
+import configureStore from './store/configureStore';
 const store = configureStore();
 
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1"}));
-store.dispatch(bugAdded({ description: "Bug 2"}));
-store.dispatch(bugAdded({ description: "Bug 3"}));
-store.dispatch(bugAdded({ description: "Bug 4"}));
-store.dispatch(bugAdded({ description: "Bug 5"}));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(userAdded({ name: "Georgi" }));
-store.dispatch(userAdded({ name: "Shéyaa" }));
-store.dispatch(bugAssigned({ bugId: 1, userId: 1 }))
-store.dispatch(bugAssigned({ bugId: 2, userId: 1 }))
-store.dispatch(bugAssigned({ bugId: 2, userId: 2 }))
-store.dispatch(bugAssigned({ bugId: 3, userId: 2 }))
-store.dispatch(bugAssigned({ bugId: 4, userId: 2 }))
-store.dispatch(bugAssigned({ bugId: 5, userId: 2 }))
+store.dispatch((dispatch, getState) => {
+  // Call an API
+  // When the promise is resolved => dispatch();
+  dispatch({ type: 'bugsReceived', bugs: [1, 2, 3] })
+  console.log(getState())
+  // If the promise is rejected => dispatch();
 
-const x = getBugsByUser(1)(store.getState())
-const y = getBugsByUser(2)(store.getState())
-
-console.log(x);
-console.log(y);
+});
--- the-ultimate-redux-course/redux-starter/src/store/configureStore.js
+++ the-ultimate-redux-course/redux-starter/src/store/configureStore.js
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from  './reducer';
 import logger from './middleware/logger';
+// import func from './middleware/func';
+
+
 
 export default function() {
   return configureStore({
     reducer,
-    middleware: [
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware(),
       logger({ destination: "console" })
-    ],
+    ]
   });
 };
 

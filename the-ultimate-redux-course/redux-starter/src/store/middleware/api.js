5ee65e6867afb7750e2ed514745a0e0d7929b3b6
Dimulski <george.dimulski@gmail.com>
2022-04-11 22:40:01 +0300

    [master] Use createActions in API middleware

--- the-ultimate-redux-course/redux-starter/src/store/middleware/api.js
+++ the-ultimate-redux-course/redux-starter/src/store/middleware/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import * as actions from '../api';
 
 const api = ({ dispatch }) => next => async action => {
-  if (action.type !== 'apiCallBegan') {
+  if (action.type !== actions.apiCallBegan.type) {
     return next(action);
   }
 
@@ -17,10 +18,19 @@ const api = ({ dispatch }) => next => async action => {
       method,
       data,
     });
-
-    dispatch({ type: onSuccess, payload: response.data });
+    // General
+    dispatch(actions.apiCallSuccess(response.data));
+    // Specific
+    if (onSuccess) {
+      dispatch({ type: onSuccess, payload: response.data });
+    }
   } catch(error) {
-    dispatch({ type: onError, payload: error })
+    // General
+    dispatch(actions.apiCallFailed(error));
+    // Specific
+    if (onError) {
+      dispatch({ type: onError, payload: error })
+    }
   }
 };
 

4eb60c33565b6eb5c3fd017a5002d8827805f4e9
Dimulski <george.dimulski@gmail.com>
2022-04-17 23:56:04 +0300

    [master] Add simple caching

--- the-ultimate-redux-course/redux-starter/src/store/bugs.js
+++ the-ultimate-redux-course/redux-starter/src/store/bugs.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 import { apiCallBegan } from './api';
+import moment from 'moment';
 
 let lastId = 0;
 
@@ -29,6 +30,7 @@ const slice = createSlice({
     bugsReceived: (bugs, action) => {
       bugs.list = action.payload;
       bugs.loading = false;
+      bugs.lastFetch = Date.now();
     },
 
     bugsRequestFailed: (bugs, action) => {
@@ -78,12 +80,31 @@ export default slice.reducer
 // Action Creators
 const url = "/bugs";
 
-export const loadBugs = () => apiCallBegan({
-  url,
-  onStart: bugsRequested.type,
-  onSuccess: bugsReceived.type,
-  onError: bugsRequestFailed.type
-})
+// () => fn(dispatch, getState)
+export const loadBugs = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.bugs;
+
+  const diffInMinutes = moment().diff(moment(lastFetch), 'minutes');
+  if (diffInMinutes < 10) {
+    return;
+  }
+
+  dispatch(
+    apiCallBegan({
+      url,
+      onStart: bugsRequested.type,
+      onSuccess: bugsReceived.type,
+      onError: bugsRequestFailed.type
+    })
+  );
+}
+
+// export const loadBugs = () => apiCallBegan({
+//   url,
+//   onStart: bugsRequested.type,
+//   onSuccess: bugsReceived.type,
+//   onError: bugsRequestFailed.type
+// })
 
 // Selector function
 // export const getUnresolvedBugs = (state) => {
